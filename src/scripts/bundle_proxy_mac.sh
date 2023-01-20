3afd397db896464ff0a488e2573412ce1a6989be
Andreas Richter <708186+richtera@users.noreply.github.com>
2023-01-20 17:09:21 -0500

    fix: Revert forced universal build assuming python is correct

--- src/scripts/bundle_proxy_mac.sh
+++ src/scripts/bundle_proxy_mac.sh
@@ -17,35 +17,27 @@ ETH2DEPOSITCLIPATH=$SCRIPTPATH/../vendors/$EDCDIR
 ETH2REQUIREMENTSPATH=$ETH2DEPOSITCLIPATH/requirements.txt
 
 PYTHONPATH=$TARGETPACKAGESPATH:$ETH2DEPOSITCLIPATH:$(python3 -c "import sys;print(':'.join(sys.path))")
-DISTBINARMPATH=$SCRIPTPATH/../../build/bin_x86
-DISTBINX86PATH=$SCRIPTPATH/../../build/bin_arm
+DISTBINPATH=$SCRIPTPATH/../../build/bin
 DISTWORDSPATH=$SCRIPTPATH/../../build/word_lists
 SRCWORDSPATH=$SCRIPTPATH/../vendors/$EDCDIR/staking_deposit/key_handling/key_derivation/word_lists
 SRCINTLPATH=$SCRIPTPATH/../vendors/$EDCDIR/staking_deposit/intl
 
-mkdir -p $DISTBINARMPATH
-mkdir -p $DISTBINX86PATH
+mkdir -p $DISTBINPATH
 mkdir -p $DISTWORDSPATH
 mkdir -p $TARGETPACKAGESPATH
 
 # Getting all the requirements
 python3 -m pip install -r $ETH2REQUIREMENTSPATH --target $TARGETPACKAGESPATH
+cd $PYINSTALLSRC
 
 # Bundling Python eth2deposit_proxy
 PYTHONPATH=$PYTHONPATH pyinstaller \
     --onefile \
-    --distpath $DISTBINX86PATH \
-    --target-arch x86_64 \
+    --distpath $DISTBINPATH \
+    --target-arch universal2 \
     --add-data "$SRCINTLPATH:staking_deposit/intl" \
     -p $PYTHONPATH \
     $SCRIPTPATH/eth2deposit_proxy.py
-
-PYTHONPATH=$PYTHONPATH pyinstaller \
-    --onefile \
-    --distpath $DISTBINARMPATH \
-    --target-arch arm64
-    --add-data "$SRCINTLPATH:staking_deposit/intl" \
-    -p $PYTHONPATH \
-    $SCRIPTPATH/eth2deposit_proxy.py
+    
 # Adding word list
 cp $SRCWORDSPATH/* $DISTWORDSPATH
