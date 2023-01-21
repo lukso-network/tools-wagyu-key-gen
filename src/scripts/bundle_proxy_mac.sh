dd694c41d2ce25b8ed099f31f2a530b2eedeaea2
Andreas Richter <708186+richtera@users.noreply.github.com>
2023-01-21 16:09:01 -0500

    fix: Local testing now works.

--- src/scripts/bundle_proxy_mac.sh
+++ src/scripts/bundle_proxy_mac.sh
@@ -21,9 +21,12 @@ DISTBINPATH=$SCRIPTPATH/../../build/bin
 DISTWORDSPATH=$SCRIPTPATH/../../build/word_lists
 SRCWORDSPATH=$SCRIPTPATH/../vendors/$EDCDIR/staking_deposit/key_handling/key_derivation/word_lists
 SRCINTLPATH=$SCRIPTPATH/../vendors/$EDCDIR/staking_deposit/intl
+DISTARCHPATH=$SCRIPTPATH/../../.build
+DISTARMPATH=$DISTARCHPATH/bin-arm64
+DISTX64PATH=$DISTARCHPATH/bin-x64
 
-mkdir -p $DISTBINPATH-x64
-mkdir -p $DISTBINPATH-arm64
+mkdir -p $DISTARMPATH
+mkdir -p $DISTX64PATH
 mkdir -p $DISTBINPATH
 mkdir -p $DISTWORDSPATH
 mkdir -p $TARGETPACKAGESPATH
@@ -35,14 +38,14 @@ python3 -m pip install -r $ETH2REQUIREMENTSPATH --target $TARGETPACKAGESPATH
 # Bundling Python eth2deposit_proxy
 PYTHONPATH=$PYTHONPATH pyinstaller \
     --onefile \
-    --distpath $DISTBINPATH \
+    --distpath $DISTARCHPATH \
     --target-arch universal2 \
     --add-data "$SRCINTLPATH:staking_deposit/intl" \
     -p $PYTHONPATH \
     $SCRIPTPATH/eth2deposit_proxy.py
-lipo -extract arm64 $DISTBINPATH/eth2deposit_proxy -output $DISTBINPATH-arm64/eth2deposit_proxy
-lipo -extract x86_64 $DISTBINPATH/eth2deposit_proxy -output $DISTBINPATH-x64/eth2deposit_proxy
-rm $DISTBINPATH/eth2deposit_proxy
+lipo -extract arm64 $DISTARCHPATH/eth2deposit_proxy -output $DISTARMPATH/eth2deposit_proxy
+lipo -extract x86_64 $DISTARCHPATH/eth2deposit_proxy -output $DISTX64PATH/eth2deposit_proxy
+
 
 # Adding word list
 cp $SRCWORDSPATH/* $DISTWORDSPATH
