616a3c613833e9168e20e51a9532a703ab4afa11
Andreas Richter <708186+richtera@users.noreply.github.com>
2023-08-10 04:13:55 -0400

    fix: Allow 2000 keys to be created.

--- src/react/components/KeyGenerationFlow/0-KeyInputs.tsx
+++ src/react/components/KeyGenerationFlow/0-KeyInputs.tsx
@@ -83,7 +83,7 @@ const KeyInputs = (props: GenerateKeysProps) => {
               type="number"
               value={props.numberOfKeys}
               onChange={updateNumberOfKeys}
-              InputProps={{ inputProps: { min: 1, max: 1000 } }}
+              InputProps={{ inputProps: { min: 1, max: 2000 } }}
               error={props.numberOfKeysError}
               helperText={ props.numberOfKeysError ? errors.NUMBER_OF_KEYS : ""}
             />
